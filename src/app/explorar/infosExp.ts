e41555e07ec6dcf3fee569a669559ba2f41692f6
Gabriel Silveira Lima <61114584347@DPEMA.BR>
2025-09-09 09:42:18 -0300

    perf: add filter infos

--- src/app/explorar/infosExp.ts
+++ src/app/explorar/infosExp.ts
@@ -1,45 +1,51 @@
 export interface Exp {
-    id: number;
-    name: string;
-    date: number;
-    description: string;
-    image: string;
+  id: number;
+  name: string;
+  date: number;
+  description: string;
+  image: string;
+  category: 'Satélites' | 'Tripuladas' | 'Sondas' | 'Telescópio';
 }
 
 export const infoExp: Exp[] = [
-    {
-        id: 1,
-        name: 'Sputnik 1',
-        date: 1957,
-        description: 'Primeiro satélite artificial da terra',
-        image: '/image/sputnik.png',
-    },
-    {
-        id: 2,
-        name: 'Apollo 11',
-        date: 1969,
-        description: 'Primeiro ação à lua',
-        image: '/image/apollo11.jpg',
-    },
-    {
-        id: 3,
-        name: 'Voyager 1 & 2',
-        date: 1977,
-        description: 'Exploração dos planetas exteriores',
-        image: '/image/voyager.png',
-    },
-    {
-        id: 4,
-        name: 'Hubble Space Telescope',
-        date: 1990,
-        description: 'Observações profundas do universo',
-        image: '/image/hubble.jpg',
-    },
-    {
-        id: 5,
-        name: 'Perseverance Mars',
-        date: 2021,
-        description: 'Rover explorando Marte em busca de vida',
-        image: '/image/mars.jpg',
-    },
-]
+  {
+    id: 1,
+    name: 'Sputnik 1',
+    date: 1957,
+    description: 'Primeiro satélite artificial da terra',
+    image: '/image/sputnik.png',
+    category: 'Satélites',
+  },
+  {
+    id: 2,
+    name: 'Apollo 11',
+    date: 1969,
+    description: 'Primeira ação à lua',
+    image: '/image/apollo11.jpg',
+    category: 'Tripuladas',
+  },
+  {
+    id: 3,
+    name: 'Voyager 1 & 2',
+    date: 1977,
+    description: 'Exploração dos planetas exteriores',
+    image: '/image/voyager.png',
+    category: 'Sondas',
+  },
+  {
+    id: 4,
+    name: 'Hubble Space Telescope',
+    date: 1990,
+    description: 'Observações profundas do universo',
+    image: '/image/hubble.jpg',
+    category: 'Telescópio',
+  },
+  {
+    id: 5,
+    name: 'Perseverance Mars',
+    date: 2021,
+    description: 'Rover explorando Marte em busca de vida',
+    image: '/image/mars.jpg',
+    category: 'Sondas',
+  },
+];
--- src/app/explorar/page.tsx
+++ src/app/explorar/page.tsx
@@ -1,51 +1,61 @@
+'use client'
+import { useState } from "react";
 import Header from "@/component/header/Header";
 import { infoData, Info } from "../explorar/infosData";
 import { Exp, infoExp } from "./infosExp";
 
+export default function Explorer() {
+  const [selectedCategory, setSelectedCategory] = useState<string>('Todas');
 
+  // Filtro condicional
+  const filteredExp = selectedCategory === 'Todas'
+    ? infoExp
+    : infoExp.filter((item) => item.category === selectedCategory);
 
-export default function Explorer(){
-    return(
+  return (
     <>
-    <Header />
+      <Header />
 
-    <main>
+      <main>
         {/* hero */}
-        <section className="flex flex-col justify-center items-center mt-40 mb-10 text-blue-100 ">
-            <h1 className="text-2xl sm:text-4xl mb-10">Exploração Espacial</h1>
-            <p className="text-base sm:text-xl mr-4 ml-4">Descubra os marcos da exploração espacial e as missões que expandiram nosso conhecimento sobre o universo</p>
+        <section className="flex flex-col justify-center items-center mt-40 mb-10 text-blue-100">
+          <h1 className="text-2xl sm:text-4xl mb-10">Exploração Espacial</h1>
+          <p className="text-base sm:text-xl mr-4 ml-4">
+            Descubra os marcos da exploração espacial e as missões que expandiram nosso conhecimento sobre o universo
+          </p>
         </section>
 
-        {/* infos  */}
-        <section className="grid grid-cols-3 md:grid-cols-5 gap-5 mt-5 mb-15 sm:mx-auto max-w-5xl mr-4 ml-4 ">
-            {infoData.map((info: Info) => (
-                <div 
-                key={info.id}
-                className="bg-slate-700 text-white rounded-xl p-2 text-center text-base md:text-xl md:w-35  lg:text-2xl w-25  flex items-center justify-center" >
-                <h2>{info.name}</h2>
-                </div>
-            ))}
+        {/* Filtros */}
+        <section className="grid grid-cols-3 md:grid-cols-5 gap-5 mt-5 mb-15 sm:mx-auto max-w-5xl mr-4 ml-4">
+          {infoData.map((info: Info) => (
+            <button key={info.id}
+              onClick={() => setSelectedCategory(info.name)}
+             className='bg-slate-700 text-white rounded-xl p-2 text-center text-base md:text-xl md:w-35 lg:text-2xl w-25 flex items-center justify-center transition duration-200'>
+              {info.name}
+            </button>
+          ))}
         </section>
 
-        {/* descobertas  */}
+        {/* Descobertas */}
         <section>
-            {infoExp.map((info: Exp) => (
-            <div  key={info.id}
-            className="bg-blue-950 text-blue-100 rounded-lg p-4 mb-5 m-5 md:mx-20 lg:mx-60 flex flex-col md:flex-row md:justify-between md:items-center" >
-
-            {/* Texto */}
-            <div className="flex flex-col  md:max-w-[30%]">
+          {filteredExp.map((info: Exp) => (
+            <div key={info.id}
+              className="bg-blue-950 text-blue-100 rounded-lg p-4 px-7 mb-14 m-5 md:mx-20 lg:mx-60 flex flex-col md:flex-row md:justify-between md:items-center">
+              
+              {/* Texto */}
+              <div className="flex flex-col md:gap-2">
                 <h2 className="text-2xl mb-2">{info.name}</h2>
                 <p className="text-base">{info.date}</p>
                 <p>{info.description}</p>
-            </div>
+              </div>
 
-            {/* Imagem */}
-                <img src={info.image} className="w-full h-auto md:h-52 mt-5 rounded-md md:mt-0 md:w-[35%] md:object-contain"/>
+              {/* Imagem */}
+              <img src={info.image}
+                className="w-full h-auto md:h-70 mt-5 rounded-md md:mt-0 md:w-[35%]" />
             </div>
-            ))}
+          ))}
         </section>
-    </main>
+      </main>
     </>
-    )
+  );
 }
